6f7b493c7d2f73fc048c92b6388f23280c3d1e7b
ibifuro odu <oduibifuro@gmail.com>
2021-12-07 09:46:01 -0500

    search for cat breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,5 +4,4 @@ request('https://api.thecatapi.com/v1/breeds/search?q=sib', function(error, resp
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   const data = JSON.parse(body);
   console.log(data[0].description);
-  console.log(typeof data);
 });
